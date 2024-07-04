23ea66b564dfedf31d8f0b05bf163aa728bd4505
Arnaud Declercq <arnaud.declercq@burohappold.com>
2024-07-04 10:50:05 +0100

    Fixing missing `;`

--- PowerPoint_Adapter/CRUD/Update/Colour.cs
+++ PowerPoint_Adapter/CRUD/Update/Colour.cs
@@ -71,8 +71,8 @@ namespace BH.Adapter.PowerPoint
             if (!string.IsNullOrEmpty(update.EdgeColour))
             {
                 Drawing.Outline outline = shape.ShapeProperties?.GetFirstChild<Drawing.Outline>() ?? shape.ShapeProperties.AppendChild(new Drawing.Outline());
-            
-                SetFillColour(outline, update.EdgeColour)
+
+                SetFillColour(outline, update.EdgeColour);
             }
             if (!string.IsNullOrEmpty(update.FillColour))
             {
