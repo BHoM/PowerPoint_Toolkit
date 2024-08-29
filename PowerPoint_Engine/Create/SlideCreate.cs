8090eff313a8ea41f6b557469a643f898340cafb
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-08-29 14:19:52 +0100

    allow slide updates to be unset when creating slides

--- PowerPoint_Engine/Create/SlideCreate.cs
+++ PowerPoint_Engine/Create/SlideCreate.cs
@@ -8,12 +8,13 @@ namespace BH.Engine.Adapters.PowerPoint
 {
     public static partial class Create
     {
-        public static SlideCreate SlideCreate(int slideNumber, string slideMasterName, string slideLayoutName, List<ISlideUpdate> slideUpdates)
+        public static SlideCreate SlideCreate(int slideNumber, string slideMasterName, string slideLayoutName, List<ISlideUpdate> slideUpdates = null)
         {
+            if (slideUpdates == null)
+                slideUpdates = new List<ISlideUpdate>();
+
             foreach (ISlideUpdate update in slideUpdates)
-            {
                 update.SlideNumber = slideNumber;
-            }
 
             SlideCreate slideCreate = new SlideCreate(slideMasterName, slideLayoutName, slideNumber, slideUpdates);
             return slideCreate;
